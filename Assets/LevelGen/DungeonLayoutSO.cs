d82d1bcb5126a75ad7825cdea7a6116dd79491e8
loof <Simon.svanstrom06@gmail.com>
2025-03-10 15:36:03 +0100

    Revert "Merge branch 'main' of https://github.com/Godlycatpictures/steamboy-2"
    This reverts commit a47b190ed21eec70e1c743d1e3b455a9cbf0f87f, reversing
    changes made to 5dd9a11acb012af33843f0ace1ae6cf14f36855f.

--- Assets/LevelGen/DungeonLayoutSO.cs
+++ Assets/LevelGen/DungeonLayoutSO.cs
@@ -1,23 +1,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-namespace needRoomData
+[CreateAssetMenu(fileName = "NewDungeonLayout", menuName = "Dungeon/Dungeon Layout")]
+public class DungeonLayoutSO : ScriptableObject
 {
-    [CreateAssetMenu(fileName = "NewDungeonLayout", menuName = "Dungeon/Dungeon Layout")]
-    public class DungeonLayoutSO : ScriptableObject
-    {
-        public string dungeonName;
-        public List<RoomData> rooms;
-    }
-
-    [System.Serializable]
-    public class RoomData
-    {
-        public int id;
-        public string roomType;
-        public List<int> connections;
-        public Vector2Int Position { get; set; }
-    }
+    public string dungeonName;
+    public List<RoomData> rooms;
 }
 
+[System.Serializable]
+public class RoomData
+{
+    public int id;
+    public string roomType; // "Start", "Normal", "Boss", etc.
+    public List<int> connections;
+}
